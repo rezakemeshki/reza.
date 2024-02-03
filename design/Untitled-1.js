930cc2295b421d28c154e37d9738cef1f72320b6
rezakemeshki <133340831+rezakemeshki@users.noreply.github.com>
2024-02-03 17:45:34 +0330

    reza

---
+++ design/Untitled-1.js
@@ -0,0 +1,47 @@
+function myreza() {
+    var ir1 = document.getElementById("pp").style.display = "none"
+    document.getElementById("img3").src = " icon-plus.svg"
+}
+
+function reza() {
+
+    document.getElementById("pp").style.display = "block"
+    document.getElementById("img3").src = " icon-minus.svg"
+}
+function myreza1() {
+    document.getElementById("text").style.display = "none"
+    document.getElementById("plus").src = " icon-plus.svg"
+}
+function reza1() {
+    document.getElementById("text").style.display = "block"
+    document.getElementById("plus").src = " icon-minus.svg"
+}
+function myreza2() {
+    document.getElementById("id12").style.display = "none"
+    document.getElementById("reza12").src = " icon-plus.svg"
+}
+
+function reza2() {
+    document.getElementById("id12").style.display = "block"
+    document.getElementById("reza12").src = " icon-minus.svg"
+}
+
+function myreza3() {
+    document.getElementById("id15").style.display = "none"
+    document.getElementById("reza13").src = " icon-plus.svg"
+}
+function reza3() {
+    document.getElementById("id15").style.display = "block"
+    document.getElementById("reza13").src = " icon-minus.svg"
+}
+
+// عوض شدن تصاویر
+// var ir = document.getElementById("img3").src = " icon-minus.svg"
+// if (ir.display == "none") {
+//     ir.src = "icon-plus.svg"
+// }
+// else {
+//     ir.display == "block"
+//     ir.src = " icon-minus.svg"
+// }
+// document.getElementById("")
